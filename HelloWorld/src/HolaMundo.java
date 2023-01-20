56412b911f281af847ce668931b419f33f86daa6
VictorHugoZaldivar <victorhugo@iteso.mx>
2023-01-20 13:34:16 -0600

    Initial commit

---
+++ HelloWorld/src/HolaMundo.java
@@ -0,0 +1,29 @@
+/**
+ * Este es el primer programa en Java que desarrollo
+ * @author Víctor Hugo Zaldívar
+ * @version 0.1
+ *
+ */
+public class HolaMundo {
+	
+	/**
+	 * Este es el método principal. No recibe parámetros
+	 * e imprime un saludo en la consola
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		
+		String s1 = " Hola Mundo ";
+		
+		System.out.println(s1);
+		
+		System.out.println(s1.length());
+		
+		System.out.println(s1.charAt(4));
+		
+		System.out.println(s1.toUpperCase()+ "   " + s1);
+
+	}
+
+}
